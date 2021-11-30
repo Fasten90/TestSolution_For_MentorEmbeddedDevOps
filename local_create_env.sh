a8d82851ba6124a2b4dd06703ea5ac3b0c02ffd7
Gabor Vizi <gabor_vizi@mentor.com>
2021-11-30 14:19:42 +0100

    App: DB connection seems OK

---
+++ local_create_env.sh
@@ -0,0 +1,3 @@
+#/c/Programs/Python310/python.exe -m venv venv
+#python -m venv venv
+venv/Scripts/pip.exe install -r requirements.txt
--- my_app.py
+++ my_app.py
@@ -1,8 +1,25 @@
-import mysql-connector-python 
+import mysql.connector
+
+
+def connect_to_db():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="secret"
+    )
+    return mydb
+
+
+def queries_the_mysql_database_for_employees_working_in_the_Production_deparment_earning_more_than_100(mydb):
+    # TODO: mydb
+    print(mydb)
+    pass
 
 
 def my_app():
-    pass
+    mydb = connect_to_db()
+    result = queries_the_mysql_database_for_employees_working_in_the_Production_deparment_earning_more_than_100(mydb)
+    print(result)
 
 
 if __name__ == '__main__':
